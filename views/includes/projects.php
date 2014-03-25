0b3d1d338e7c60fdc621b1c4b571f93f2336f237
Leo Jeremiah Vicente <jemnuineuron@live.com>
2014-03-25 11:37:22 +0800

    homepage projects by @marky939

--- views/includes/projects.php
+++ views/includes/projects.php
@@ -1,2 +1,17 @@
-<p>
-Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna  </p>
+<div class="projects-container">
+	<div class="projects-thumbnail">
+		<img src="img/thumbnail-projects.jpg">
+	</div>
+	<p class="projects-caption">
+	Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna tincidunt ut.  </p>
+	<a href="#">Read More »</a>
+</div>
+
+<div class="projects-container">
+	<div class="projects-thumbnail">
+		<img src="img/thumbnail-projects.jpg">
+	</div>
+	<p class="projects-caption">
+	Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna tincidunt ut.  </p>
+	<a href="#">Read More »</a>
+</div>
